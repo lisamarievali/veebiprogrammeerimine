0c96fc2726c0bbd19c57a194c65d8267d259cedc
Lisa-Marie Väli <lisamarievali@users.noreply.github.com>
2018-09-18 11:33:39 +0300

    Lisatud failid

--- tund_3/index1.php
+++ tund_3/index1.php
@@ -4,8 +4,14 @@
   $lastName = "Väli";
   $dateToday = date("d.m.Y");
   $hourNow = date("G");
+  $weekdayNow = date("N");
+  $weekdayNamesET = ["esmaspäev", "teisipäev", "kolmapäev", "neljapäev", "reede", "laupäev", "pühapäev"];
+  echo $weekdayNamesET[1];
+  //var_dump($weekdayNamesET)[1];
+  //$weekdayNow = date("N"):
+  //echo $weekdayNow;
   $partOfDay = "";
-  if ($hourNow < 8){
+  if ($hourNow < 8) {
 	 $partOfDay = "varajane hommik";
   }
  if ($hourNow >= 8 and $hourNow < 16){
@@ -14,7 +20,14 @@
  if ($hourNow >= 16){
 	 $partOfDay = "vaba aeg"; 
  }
+	 $picNum = mt_rand(2, 43);
+	 //echo $picNum;
+	 $picURL = "http://www.cs.tlu.ee/~rinde/media/fotod/TLU_600x400/tlu_";
+	 $picEXT = ".jpg";
+	 $picFile = $picURL .$picNum .$picEXT;
+	 
 	 ?>
+	 
 <!DOCTYPE html>
 <html>
 <head>
@@ -35,17 +48,23 @@
   ?>
 , IF18</h1>
 <p>See leht on loodud <a href="http://www.tlu.ee" target="_blank">TLÜ<a> õppetöö raames, ärge pange pahaks, kui tundub algeline, siin pole tõsiseltvõetavat sisu!</p>
+
+<p>Tundides tehtu: <a href="photo.php"> photo.php </a></p>
+
 <?php
-  echo "<p>Tänane kuupäev on: " .$dateToday .".</p>\n";
+  //echo "<p>Tänane kuupäev on: " .$dateToday .".</p>\n";
+  echo "<p>Täna on " .$weekdayNamesET[$weekdayNow -1] .", " .$dateToday . ".</p>\n";
   echo "<p>Lehe avamise hetkel oli kell " .date("H:i:s") .". Käes oli " .$partOfDay .".</p> \n";
   ?>
 <p>Seoses kodutööga lisan siia veel ühe lause. <p>
 
+<!--<img src="http://greeny.cs.tlu.ee/~rinde/veebiprogrammeerimine2018s/tlu_terra_600x400_1.jpg" alt="TLÜ Terra õppehoone">-->
+   <!--<img src="../../../~rinde/veebiprogrammeerimine2018s/tlu_terra_600x400_1.jpg" alt="TLÜ Terra õppehoone">-->
+   
+  <img src="<?php echo $picFile; ?>" alt="TLÜ hoone">
 
-<!--<img src="http://greeny.cs.tlu.ee/~rinde/veebiprogrammeerimine2018s/tlu_terra_600x400_3.jpg" alt="TLU Terra õppehoone">-->
-
-<img src="../../../~rinde/veebiprogrammeerimine2018s/tlu_terra_600x400_3.jpg" alt="TLU Terra õppehoone">
 <p> Mul on ka sõber, kes teeb oma <a href="../../../~emmatae" target="_blank">veebi<a> </p>
+
  
 </body>
 </html>
